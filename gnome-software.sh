629d45f640287f7fdd9786ca55608d2f6c382e4c
sppotted <124325201+sppotted@users.noreply.github.com>
2023-02-02 20:06:02 -0700

    Update gnome-software.sh

--- gnome-software.sh
+++ gnome-software.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Disable gnome-software autostart
-rm -f "/etc/dxg/autostart/org.gnome.Software.desktop"
+sudo rm -f "/etc/xdg/autostart/org.gnome.Software.desktop"
 
 # Disable gnome-software automatic updates
 dconf write /org/gnome/software/download-updates false
